660bd192fab060bbf33d6f7067511caa23db98e3
wangonya <kwangonya@gmail.com>
2019-07-15 04:40:05 +0300

    add export to sqlite functionality

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import json
 import csv
+import sqlite3 as sq
 
 from halo import Halo
 
@@ -54,5 +55,42 @@ def export_json_csv(data, _format, limit):
                     csv_writer.writerow(header)
                     count += 1
                 csv_writer.writerow(article_data.values())
+    elif _format == "sqlite":
+        conn = sq.connect('data.db')
+        c = conn.cursor()
+        columns = list(data[0].keys())
+        columns.remove('tagList')
+        columns.remove('comments')
+        c.execute("""
+                    CREATE TABLE IF NOT EXISTS
+                    articles (
+                    id integer primary key,
+                    likes integer,
+                    dislikes integer,
+                    like_popularity integer,
+                    description text,
+                    rating integer,
+                    title text,
+                    slug text,
+                    body text,
+                    created text,
+                    updated text,
+                    reading_time text,
+                    reads integer,
+                    views integer,
+                    image text,
+                    author integer
+                    )
+                    """)
+        query = """
+                INSERT OR REPLACE INTO articles values
+                (?{})
+                """.format(",?" * (len(columns)-1))
+
+        for article_data in data:
+            article_data = tuple(article_data[c] for c in columns)
+            c.execute(query, article_data)
+            conn.commit()
+        c.close()
 
     spinner.succeed("Exported to {} 🚀".format(_format))
